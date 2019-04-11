f737939c8a5ffc8719a39c6311601fc6f7dc59f2
Ana Bello <anamariabellopena@MacBook-Air-de-Ana.local>
2019-04-11 12:10:51 -0500

    Final

--- BelloAnaS12C1.mk
+++ BelloAnaS12C1.mk
@@ -6,14 +6,16 @@ plot1.pdf : plotdatos1.py datos.dat
 	python plotdatos1.py
 datos.dat : a.out
 	./a.out>> datos.dat
-a.out : makedatos.cpp
-	g++ makedatos.cpp
-plot.pdf : plotdatos.py datos.datos
+a.out : makedatos1.cpp
+	g++ makedatos1.cpp
+plot.pdf : plotdatos.py datos.dat
 	python plotdatos.py
 datos.dat : a.out
 	./a.out>> datos.dat
-a.out : makedatos.cpp
-	g++ makedatos.cpp
+a.out : makedatos.py
+	python makedatos.py
 
 
 
+#Comentario: No estoy segura si para el plot .pdf cambia porque los datos los proporciona un .py. 
+#En ese caso no sabría si es con a.out.
